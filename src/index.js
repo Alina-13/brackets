b38d88fe0883c1cea8891135c432176a75603396
Alina-13 <alina.za13@gmail.com>
2022-08-09 20:18:11 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+       let store = [];
+   for (let bracket of str) {
+           let bracketsIndex = brackets.indexOf(bracket)
+
+           if (bracketsIndex % 2 === 0) {
+               
+               if (bracket === brackets[bracketsIndex + 1] && store[store.length - 1] === bracketsIndex){
+                  store.pop();
+               } else if (bracket === brackets[bracketsIndex + 1] && store[store.length - 1] !== bracketsIndex) {
+                   store.push(bracketsIndex)
+               }
+               else{
+                 store.push(bracketsIndex)
+               }
+           } 
+           else {
+               if (store.pop() !== bracketsIndex-1){
+                   return false;
+               }
+           }
+           
+       }
+       return store.length === 0;
 }
